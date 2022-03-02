b50aa0b06a8eae0b69cf29bcf2e631b1454a7d73
gkumar9891 <gauravkumargk9892@gmail.com>
2022-03-02 16:14:39 +0530

    update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MessangerArea />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
+import MessangerArea from "./components/Messanger.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MessangerArea,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Messanger.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="messanger">
+    <div>
+     <div class="message">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, ipsum?
+     </div>
+    </div>
+    <div class="message">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius possimus
+      iste deserunt eveniet aliquid voluptas temporibus aperiam doloremque vitae
+      qui?
+    </div>
+    <div v-for="(message, index) in messages" :key="index">
+        <span  class="message"> {{ message}} </span>
+    </div>
+
+    <form>
+        <textarea v-model="message" ></textarea>
+        <button type="button" @click="addMessage">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MessamgerArea',
+    data() {
+        return  {
+            message: '',
+            messages: []
+        }
+    },
+    methods: {
+        addMessage() {
+            this.messages.push(this.message);
+            this.message = '';
+        }
+    }
+};
+</script>
+
+<style scoped>
+.messanger {
+  width: 80%;
+  height: calc(100vh - 100px);
+  margin: auto;
+  background: #000;
+  overflow-y: auto;
+  text-align: left;
+  padding: 30px;
+
+}
+
+.message {
+  padding: 5px;
+  background-color: aqua;
+  text-align: left;
+  display: inline-block;
+  margin-bottom: 10px;
+}
+</style>
